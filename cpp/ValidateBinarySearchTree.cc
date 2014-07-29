4277eab2b5dcdc0c6428016aecb78ba28a222df3
Jiankuan Xing <speedfirst@gmail.com>
2014-07-30 00:32:46 +0800

    Update ValidateBinarySearchTree.cc

--- cpp/ValidateBinarySearchTree.cc
+++ cpp/ValidateBinarySearchTree.cc
@@ -3,14 +3,10 @@ namespace ValidateBinarySearchTree {
   public:
   
       bool doIsValidBST(TreeNode *root, int lower, int upper) {
-        bool res;
-        if (root->val <= lower || root->val >= upper) {
-            return false;
-        }
-        
-        return (root->left == NULL || doIsValidBST(root->left, lower, root->val)) &&
-               (root->right == NULL || doIsValidBST(root->right, root->val, upper));
-    }
+          return (lower < root->val && root->val < upper) &&
+                 (root->left == NULL || doIsValidBST(root->left, lower, root->val)) &&
+                 (root->right == NULL || doIsValidBST(root->right, root->val, upper));
+      }
       
       bool isValidBST(TreeNode *root) {
           if (root == NULL) {
