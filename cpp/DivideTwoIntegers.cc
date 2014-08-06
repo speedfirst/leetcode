2bc88166a116279798a99e1b23f628cb490cd2d3
Jiankuan Xing <speedfirst@gmail.com>
2014-08-07 00:45:21 +0800

    Create DivideTwoIntegers.cc

---
+++ cpp/DivideTwoIntegers.cc
@@ -0,0 +1,25 @@
+namespace DivideTwoIntegers {
+class Solution {
+  public:
+      int divide(int dividend, int divisor) {
+          int res = 0, multior = 0;
+          long long a = abs((long long)dividend);
+          long long b = abs((long long)divisor);
+          int i = 0;
+          while(a >= b){
+              i++;
+              cout<<a<<endl;
+              if(a >= (b<<multior)) {
+                  res += (1<<multior);
+                  a -= (b<<multior);
+                  ++multior;
+              }
+              else {
+                  multior = 0;
+              }
+          }
+          cout<<"total loop:"<<i<<endl;
+          return ((dividend^divisor)>>31)? 0-res:res;
+      }
+  };
+}
