30b2f05de20d74174242a23773ad129080c08045
Jiankuan Xing <speedfirst@gmail.com>
2014-08-13 23:59:01 +0800

    add solution without using extra space to find longest palindrome

--- cpp/LongestPalindromicSubstring.cc
+++ cpp/LongestPalindromicSubstring.cc
@@ -1,6 +1,6 @@
 namespace LongestPalindromicSubstring {
     
-    // this is a dp solution, complexity is O(n^2)
+    // this is a dp solution, both time and space complexity are O(n^2)
     class Solution1 {
     public:
         string longestPalindrome(string s) {
@@ -21,6 +21,48 @@ namespace LongestPalindromicSubstring {
                 dp[i][i] =1;  
             }  
             return s.substr(start, end - start + 1);  
+        }
+    };
+    
+    
+    // This solution enumerates every possible center and try to expand it to a palindrome
+    // time complexity is O(n^2), space complexity is O(1)
+    class Solution {
+    public:
+
+        // search palindrome from a center
+        void searchPalindrome(string& s, int startPos1, int startPos2, int& start, int& end) {
+            int i = startPos1, j = startPos2;
+            while (i >= 0 && j < s.size()) {
+                if (s[i] == s[j]) {
+                    i--;
+                    j++;
+                } else {
+                    break;
+                }
+            }
+            start = (i + 1);
+            end = (j - 1);
+        }
+        
+        string longestPalindrome(string s) {
+            if (s.empty()) return s;
+            int start, end;
+            int maxStart = 0, maxEnd = 0;
+            for (int i = 0; i < s.size(); i++) {
+                searchPalindrome(s, i, i, start, end); // try search by center s[i]
+                if (end - start > maxEnd - maxStart) {
+                    maxStart = start;
+                    maxEnd = end;
+                }
+                
+                searchPalindrome(s, i, i + 1, start, end); // try search by gap between s[i], s[i + 1]
+                if (end - start > maxEnd - maxStart) {
+                    maxStart = start;
+                    maxEnd = end;
+                }
+            }
+            return s.substr(maxStart, maxEnd - maxStart + 1);  
         }
     };
 }
