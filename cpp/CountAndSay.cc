a97e10af7fd3f488091be78ffa2d83d9f0042966
Jiankuan Xing <speedfirst@gmail.com>
2014-08-15 09:59:36 +0800

    Create CountAndSay.cc

---
+++ cpp/CountAndSay.cc
@@ -0,0 +1,37 @@
+namespace CountAndSay {
+  class Solution {
+  public:
+      string countAndSay(int n) {
+          if (n == 0) {
+              return "";
+          }
+          
+          if (n == 1) {
+              return "1";
+          }
+          
+          string s = countAndSay(n - 1);
+          
+          char curChar = s[0];
+          int count = 1;
+          int i = 1;
+          string res = "";
+          while (i < s.size()) {
+              if (s[i] == curChar) {
+                  count++;
+              } else {
+                  res += to_string(count);
+                  res += curChar;
+                  
+                  // reset
+                  curChar = s[i];
+                  count = 1;
+              }
+              i++;
+          }
+          res += to_string(count);
+          res += curChar;
+          return res;
+      }
+  };
+}
