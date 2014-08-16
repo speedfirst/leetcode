26d01864a0c6131e990b9af877fa6160ea2719ba
Jiankuan Xing <speedfirst@gmail.com>
2014-08-16 14:01:09 +0800

    Update SearchInRotatedSortedArray.cc

--- cpp/SearchInRotatedSortedArray.cc
+++ cpp/SearchInRotatedSortedArray.cc
@@ -1,1 +1,33 @@
 namespace SearchInRotatedSortedArray {
+    class Solution {
+    public:
+        int search(int A[], int n, int target) {
+            if (n == 0) {
+                return -1;
+            }
+    
+            int left = 0, right = n - 1;
+            while (left <= right) {
+                int mid = ((right - left) >> 1) + left;
+                if (A[mid] == target) {
+                    return mid;
+                }
+                if (A[mid] >= A[left]) {
+                    if (target >= A[left] && target < A[mid]) {
+                        right = mid - 1;
+                    } else {
+                        left = mid + 1; 
+                    }
+                } else {
+                    if (target <= A[right] && target > A[mid]) {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+            }
+    
+            return -1;
+        }
+    };
+}
