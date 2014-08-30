4fd604e5d4037bbd866f92e3dca895570a7b4195
Jiankuan Xing <speedfirst@gmail.com>
2014-08-30 20:41:46 +0800

    Create RotateImage.cc

---
+++ cpp/RotateImage.cc
@@ -0,0 +1,19 @@
+namespace RotateImage {
+    class Solution {
+    public:
+        void rotate(vector<vector<int> > &matrix) {
+            int n = matrix.size();
+            int len = n;
+            for (int i = 0; i < n / 2; i++) {
+                for (int j = i; j < len - 1 + i; j++) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[n - 1 - j][i];
+                    matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                    matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                    matrix[j][n - 1 - i] = temp;
+                }
+                len -= 2;
+            }
+        }
+    };
+}
