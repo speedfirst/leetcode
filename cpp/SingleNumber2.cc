15ec201817eaecd828d54439445f96d3fd4f7eae
Jiankuan Xing <speedfirst@gmail.com>
2014-08-30 21:06:45 +0800

    Create SingleNumber2.cc

---
+++ cpp/SingleNumber2.cc
@@ -0,0 +1,20 @@
+// https://oj.leetcode.com/problems/single-number-ii/
+namespace SingleNumber2 {
+  class Solution {
+  public:
+      int singleNumber(int A[], int n) {
+          int ones = 0;
+          int twos = 0;
+          int threes = 0;
+          for (int i = 0; i < n; i++) {
+              twos |= (ones & A[i]);
+              ones ^= A[i];
+              threes = (ones & twos);
+              ones &= ~threes;
+              twos &= ~threes;
+          }
+          
+          return ones;
+      }
+  };
+}
