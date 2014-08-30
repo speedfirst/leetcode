e7c672ee540047c7a3c69f59f4ba4cc7ee2bd101
Jiankuan Xing <speedfirst@gmail.com>
2014-08-30 20:49:53 +0800

    Create GrayCode.cc

---
+++ cpp/GrayCode.cc
@@ -0,0 +1,14 @@
+namespace GrayCode {
+    class Solution {
+    public:
+        vector<int> grayCode(int n) {
+            vector<int> res;
+            int size = (1 << n);
+            for (int i = 0; i < size; i++) {
+                res.push_back((i >> 1) ^ i);
+            }
+            
+            return res;
+        }
+    };
+}
