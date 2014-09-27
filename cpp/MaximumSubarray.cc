caff0bb913d8bae1e799799bb35a53332c0203fc
Jiankuan Xing <speedfirst@gmail.com>
2014-09-27 19:44:42 +0800

    Update MaximumSubarray.cc

--- cpp/MaximumSubarray.cc
+++ cpp/MaximumSubarray.cc
@@ -1,5 +1,6 @@
 // https://oj.leetcode.com/problems/maximum-subarray/
 namespace MaximumSubarray {
+    // O(n) solution
     class Solution {
     public:
         int maxSubArray(int A[], int n) {
@@ -21,6 +22,54 @@ namespace MaximumSubarray {
             
             return max;
             
+        }
+    };
+    
+    // Divide & Conquer Solution, O(nLog(n))
+    class Solution1 {
+    public:
+        int maxCrossSubArray(int A[], int left, int mid, int right) {
+            int maxLeftSum = INT_MIN;
+            int maxRightSum = INT_MIN;
+            
+            // calc left sum
+            int sum = 0;
+            for (int i = mid; i >= left; i--) {
+                sum += A[i];
+                if (sum > maxLeftSum) {
+                    maxLeftSum = sum;
+                }
+            }
+            
+            // calc right sum
+            sum = 0;
+            for (int i = mid + 1; i <= right; i++) {
+                sum += A[i];
+                if (sum > maxRightSum) {
+                    maxRightSum = sum;
+                }
+            }
+            
+            return maxLeftSum + maxRightSum;
+        }
+        
+        int maxOneSideSubArray(int A[], int left, int right) {
+            if (left == right) {
+                return A[left];
+            }
+            
+            int mid = (left + right) / 2;
+            
+            return max(maxCrossSubArray(A, left, mid, right),
+                    max(maxOneSideSubArray(A, left, mid), maxOneSideSubArray(A, mid + 1, right)));
+        }
+        
+        int maxSubArray(int A[], int n) {
+            if (n == 0) {
+                return 0;
+            }
+            
+            return maxOneSideSubArray(A, 0, n - 1);
         }
     };
 }
