120e13f53abbb6c77ca7f12514496bfbf998cb16
Jiankuan Xing <speedfirst@gmail.com>
2014-10-31 20:35:23 +0800

    Update DivideTwoIntegers.cc

--- cpp/DivideTwoIntegers.cc
+++ cpp/DivideTwoIntegers.cc
@@ -3,7 +3,7 @@ class Solution {
   public:
       int divide(int dividend, int divisor) {
           int res = 0, multior = 0;
-          long long a = abs((long long)dividend);
+          long long a = abs((long long)dividend); // Note we have to use long long because abs(INT_MIN) is also INT_MIN
           long long b = abs((long long)divisor);
           int i = 0;
           while(a >= b){
