64bff2b744eb53580561938509a27177500efa60
Jiankuan Xing <speedfirst@gmail.com>
2014-11-05 11:27:21 +0800

    Add new solution to this problem

--- cpp/ReverseNodesInKGroup.cc
+++ cpp/ReverseNodesInKGroup.cc
@@ -58,4 +58,64 @@ namespace ReverseNodesInKGroup {
             return dummy.next;
         }
     };
+    
+    //-------------------------------------------------------------------------------
+    // This new solution separate the reverse code and main control code
+    //-------------------------------------------------------------------------------
+    struct ReverseData {
+        ListNode * prev2;
+        ListNode * cur2;
+        int count;
+    };
+    
+    class Solution {
+    public:
+        ReverseData reverse(ListNode * prev2, int k) {
+            ListNode * cur2 = prev2->next;
+            int count = 0;
+            while (count < k && cur2 != NULL) {
+                ListNode * next = cur2->next;
+                cur2->next = prev2;
+                prev2 = cur2;
+                cur2 = next;
+                count++;
+            }
+            
+            ReverseData rd;
+            rd.prev2 = prev2;
+            rd.cur2 = cur2;
+            rd.count = count;
+            return rd;
+        }
+        
+        ListNode *reverseKGroup(ListNode *head, int k) {
+            if (head == NULL || head->next == NULL) {
+                return head;
+            }
+            
+            if (k <= 1) {
+                return head;
+            }
+            
+            ListNode dummy(-1);
+            dummy.next = head;
+            ListNode * prev = &dummy;
+            bool lastRun = false;
+            while (true)
+                ListNode * cur = prev->next;
+                if (cur == NULL) break;
+                ReverseData rd = reverse(prev, k);
+                prev->next = rd.prev2;
+                cur->next = rd.cur2;
+                if (lastRun) break;
+                if (rd.count < k) {
+                    lastRun = true; // don't move forward, set last run flag and revert it back
+                } else {
+                    prev = cur; // move forward
+                }
+            }
+            
+            return dummy.next;
+        }
+    };
 }
