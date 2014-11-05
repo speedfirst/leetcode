ea2de9241a2946d281e2b6b24954e5869ff4de2b
Jiankuan Xing <speedfirst@gmail.com>
2014-11-05 16:34:41 +0800

    Add DP solution

--- cpp/RegularExpressionMatching.cc
+++ cpp/RegularExpressionMatching.cc
@@ -60,4 +60,41 @@ namespace RegularExpressionMatching {
             return checkIsMatch(s, 0, p, 0);
         }
     };
+    
+    // ----------------------------------------------------------------
+    // add DP solution
+    // ----------------------------------------------------------------
+    class Solution {
+    public:
+        bool isMatch(const char *s, const char *p) {
+            int sLen = strlen(s);
+            int pLen = strlen(p);
+            
+            vector<vector<bool> > dp(sLen + 1, vector<bool>(pLen + 1, false));
+            dp[0][0] = true;
+            
+            for (int i = 1; i <= pLen; i++) {
+                if (p[i - 1] == '*') dp[0][i] = dp[0][i - 2]; // we assume that the invalid pattern with only a "*" won't appear
+            }
+            
+            for (int i = 1; i <= sLen; i++) {
+                for (int j = 1; j <= pLen; j++) {
+                    char cS = s[i - 1];
+                    char cP = p[j - 1];
+                    if (cS == cP || cP == '.') {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    } else if (cP == '*') {
+                        char cPPrev = p[j - 2];
+                        if (cPPrev != cS && cPPrev != '.') {
+                            dp[i][j] = dp[i][j - 2];
+                        } else {
+                            dp[i][j] = dp[i][j - 2] | dp[i - 1][j];
+                        }
+                    }
+                }
+            }
+            
+            return dp[sLen][pLen];
+        }
+    };
 }
