fdebe3655c8a5c62373a64230be12117988e71cf
Jiankuan Xing <speedfirst@gmail.com>
2014-11-05 18:42:00 +0800

    Update WordBreak.cc

--- cpp/WordBreak.cc
+++ cpp/WordBreak.cc
@@ -2,59 +2,38 @@
 namespace WordBreak {
     class Solution {
     public:
-        bool wordBreak(string s, unordered_set<string> &dict) {
-            
-            if (s.length() == 0) {
-                return true;
-            }
-            
-            if (dict.size() == 0) {
-                return false;
-            }
-            
-            // 1. find the words in dict with min and max length
-            unordered_set<string>::const_iterator it = dict.begin();
+        pair<int, int> findMinLenAndMaxLen(unordered_set<string> &dict) {
             int minLen = INT_MAX;
             int maxLen = INT_MIN;
-            for (; it != dict.end(); it++) {
-                int len = (*it).length();
-                if (len < minLen) {
-                    minLen = len;
-                }
-                
-                if (len > maxLen) {
-                    maxLen = len;
-                }
+            for (unordered_set<string>::iterator it = dict.begin(); it != dict.end(); it++) {
+                const string & cur = *it;
+                minLen = min((int)cur.size(), minLen);
+                maxLen = max((int)cur.size(), maxLen);
             }
+            return make_pair(minLen, maxLen);
+        }
+        
+        bool wordBreak(string s, unordered_set<string> &dict) {
+            if (s.empty()) return false;
+            if (dict.empty()) return false;
             
-            
-            // 2. run dp
-            bool * dp = new bool[s.length() + 1];
-            memset(dp, 0, sizeof(bool) * (s.length() + 1));
+            pair<int, int> m = findMinLenAndMaxLen(dict);
+            int minLen = m.first;
+            int maxLen = m.second;
+            vector<bool> dp = vector<bool>(s.size() + 1, false);
             dp[0] = true;
-            for (int i = 1; i <= s.length() - minLen + 1; i++) {
-                
-                if (dp[i - 1]) {
-                    for (int len = minLen; len <= maxLen; len++) {
-                        int endPos = i + len - 1;
-                        if (endPos > s.length()) {
-                            break;
-                        }
-                        
-                        if (dp[endPos] == true) {
-                            continue;
-                        }
-                        
-                        string str = s.substr(i - 1, len);
-                        if (dict.find(str) != dict.end()) {
-                            dp[endPos] = true;
-                            continue;
-                        }
+            for (int i = 1; i <= s.size(); i++) {
+                if (!dp[i - 1]) continue;
+                for (int len = minLen; len <= maxLen; len++) {
+                    if (i + len - 1 > s.size()) break;
+                    string cur = s.substr(i - 1, len);
+                    if (dict.find(cur) != dict.end()) {
+                        dp[i + len - 1] = true;
                     }
                 }
             }
             
-            return dp[s.length()];
+            return dp[s.size()];
         }
     };
 }
