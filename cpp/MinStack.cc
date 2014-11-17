1ac71be50860d3ba16c73f7f3b05b0a9998556b1
Jiankuan Xing <speedfirst@gmail.com>
2014-11-17 13:58:58 +0800

    Create MinStack.cc

---
+++ cpp/MinStack.cc
@@ -0,0 +1,33 @@
+class MinStack {
+private:
+    stack<int> S;
+    stack<int> MIN_S;
+public:
+    void push(int x) {
+        S.push(x);
+        if (MIN_S.empty() || x <= MIN_S.top()) {
+            MIN_S.push(x);
+        }
+    }
+
+    void pop() {
+        if (S.empty()) return;
+        int element = S.top();
+        S.pop();
+        if (element == MIN_S.top()) MIN_S.pop();
+    }
+
+    int top() {
+        if (S.empty()) {
+            return -1;
+        }
+        return S.top();
+    }
+
+    int getMin() {
+        if (MIN_S.empty()) {
+            return -1;
+        }
+        return MIN_S.top();
+    }
+};
