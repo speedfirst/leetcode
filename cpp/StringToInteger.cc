5b5946cd5c7536012c2c4958a612c7314bc399eb
Jiankuan Xing <speedfirst@gmail.com>
2014-12-01 23:51:49 +0800

    Improve code to make it concise

--- cpp/StringToInteger.cc
+++ cpp/StringToInteger.cc
@@ -1,117 +1,53 @@
-namespace StringToInteger {
-    class Solution {
-    public:
+// https://oj.leetcode.com/problems/string-to-integer-atoi/
+class Solution {
+public:
 
-        bool isWhitespace(char c) {
-            if (c == ' ' ||
-                c == '\r' ||
-                c == '\n' ||
-                c == '\t') {
-                return true;
-            } else {
-                return false;
-            }
+    bool isWhitespace(char c) {
+        if (c == ' ' ||
+            c == '\r' ||
+            c == '\n' ||
+            c == '\t') {
+            return true;
+        } else {
+            return false;
         }
-        
-        bool isNum(char c) {
-            if ('0' <= c && c <= '9') {
-                return true;
-            } else {
-                return false;
-            }
-        }
-        
-        bool isPosOrNeg(char c) {
-            if (c == '+' || c == '-') {
-                return true;
-            } else {
-                return false;
-            }
+    }
+    
+    bool isNum(char c) {
+        return ('0' <= c && c <= '9') ? true : false;
+    }
+    
+    int maxDiv10 = INT_MAX / 10;
+    int maxLastDigit = INT_MAX - INT_MAX / 10 * 10;
+    
+    int atoi(const char *str) {
+        if (str == NULL) return 0;
+        int pos = 0;
+        do {
+            if (!isWhitespace(str[pos])) break;
+            pos++;
+        } while (true);
+        int sign = 1;
+        if(str[pos] == '+') {
+            pos++;
+        } else if (str[pos] == '-') {
+            sign = -1;
+            pos++;
+        } else if (!isNum(str[pos])) {
+            return 0;
         }
 
-        int genResult(int res, bool isNeg) {
-            if (res < 0) {
-                // overflow
-                if (isNeg) {
-                    return INT_MIN;
-                } else {
-                    return INT_MAX;
-                }
-            } 
-            
-            if (isNeg) {
-                res *= -1;
+        int num = 0;
+        do {
+            if (!isNum(str[pos])) return num * sign;
+            int digit = str[pos] - '0';
+            if (num > maxDiv10 || (num == maxDiv10 && digit > maxLastDigit)) {
+                return sign == 1 ? INT_MAX : INT_MIN;
             }
-            
-            return res;
-        }
+            num = num * 10 + digit;
+            pos++;
+        } while (true);
         
-        int genOverflowResult(bool isNeg) {
-            if (isNeg) {
-                return INT_MIN;
-            } else {
-                return INT_MAX;
-            }
-        }
-        
-        int atoi(const char *str) {
-            int res = 0;
-            if (str == NULL) {
-                return res;
-            }
-            
-            int len = strlen(str);
-            if (len == 0) {
-                return res;
-            }
-            
-            const int stat_prenum = 0;
-            const int stat_posneg = 1;
-            const int stat_num = 2; 
-            const int threshold = INT_MAX / 10; // for overflow detection
-            
-            int st = stat_prenum;
-            int i = 0;
-            bool isNeg = false;
-            while (i < len) {
-                char c = str[i];
-                switch (st) {
-                    case stat_prenum:
-                        if (isWhitespace(c)) {i++; continue;}
-                        else if (isPosOrNeg(c)) {st = stat_posneg; isNeg = (c == '-'); i++; continue;}
-                        else if (isNum(c)) {st = stat_num; res = (c - '0'); i++; continue;}
-                        else {return res;}
-                        break;
-                    case stat_posneg:
-                        if (isNum(c)) {st = stat_num; res = (c - '0'); i++; continue;}
-                        else {return res;}
-                        break;
-                    case stat_num:
-                        if (isNum(c)) {
-                            // judge overflow
-                            if (threshold < res ) {
-                                return genOverflowResult(isNeg);
-                            }
-                            res *= 10;
-                            res += (c - '0');
-                            if (res < 0) { 
-                                // judge overlfow again
-                                // you can't judge overflow only by judging it's negative or not
-                                // because an overlfowed number after * 10
-                                // might still be positive. This is why we use "threshold" above.
-                                return genOverflowResult(isNeg);
-                            }
-                            i++;
-                            continue;}
-                        else { return genResult(res, isNeg);}
-                        break;
-                    default:
-                        break;
-                        // do nothing
-                }
-            }
-            
-            return genResult(res, isNeg);
-        }
-    };
-}
+        return num * sign;
+    }
+};
