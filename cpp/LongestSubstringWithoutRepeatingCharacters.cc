4fe3ddbe8ba164fc587bd66258c91bec81efc777
Jiankuan Xing <speedfirst@gmail.com>
2014-12-02 13:54:13 +0800

    Update LongestSubstringWithoutRepeatingCharacters.cc

--- cpp/LongestSubstringWithoutRepeatingCharacters.cc
+++ cpp/LongestSubstringWithoutRepeatingCharacters.cc
@@ -1,34 +1,24 @@
-namespace LongestSubstringWithoutRepeatingCharacters {
-    class Solution {
-    public:
-        // complexity worst O(2n)
-        int lengthOfLongestSubstring(string s) {
-            int appearedPos[256]; // we assume the character range is 0 ~ 255
-            memset(appearedPos, -1, 256 * sizeof(int));
-            
-            int i = 0;
-            int len = 0;
-            int maxLen = 0;
-            while (i < s.size()) {
-                char c = s[i];
-                int pos = appearedPos[c];
-                if (pos == -1) {
-                    // this char hasn't appeared
-                    appearedPos[c] = i;
-                    i++;
-                    len++;
-                } else {
-                    // this char has appeared, reset
-                    i = pos + 1;
-                    if (len > maxLen) {
-                        maxLen = len;
-                    }
-                    len = 0;
-                    memset(appearedPos, -1, 256 * sizeof(int));
-                }
+// https://oj.leetcode.com/problems/longest-substring-without-repeating-characters/
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.empty()) return 0;
+        int arr[256];
+        memset(arr, -1, sizeof(arr));
+        int curPos = 0;
+        int startPos = 0;
+        int maxLen = 0;
+        while (curPos < s.size()) {
+            char c = s[curPos];
+            if (arr[c] >= startPos) {
+                startPos = arr[c] + 1;
             }
-            
-            return max(len, maxLen);
+            arr[c] = curPos;
+            maxLen = max(maxLen, curPos - startPos + 1);
+            curPos++;
         }
-    };
-}
+        
+        return maxLen;
+    }
+
+};
