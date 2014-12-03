5beb87ffbc5249f0ac89abc0d2135a64cb23bb55
Jiankuan Xing <speedfirst@gmail.com>
2014-12-03 17:29:09 +0800

    implement a more elegant code

--- cpp/ReadNCharactersGivenRead4_2.cc
+++ cpp/ReadNCharactersGivenRead4_2.cc
@@ -1,44 +1,36 @@
 // https://oj.leetcode.com/problems/read-n-characters-given-read4-ii-call-multiple-times/
 
-// Forward declaration of the read4 API.
 int read4(char *buf);
 
 class Solution {
 public:
-        char tmpBuf[4];
-        int tmpStart = 0;
-        int tmpEnd = 0;
-        bool eof = false;
+
+    char myBuf[4];
+    int myBufStart = 0;
+    int myBufLen = 0;
+    
     /**
      * @param buf Destination buffer
      * @param n   Maximum number of characters to read
      * @return    The number of characters read
      */
     int read(char *buf, int n) {
-        if (n == 0) return 0;
-
-        int count = 0;
+        int readBytes = 0;
+        int bufStart = 0;
         bool eof = false;
-        do {
-            int tmpLen = tmpEnd - tmpStart;
-            int copyCount = min(tmpLen, n - count);
-            if (copyCount > 0) {
-                memcpy(buf + count, tmpBuf + tmpStart, copyCount);
-                count += copyCount;
-                tmpStart += copyCount;
+        while (!eof && readBytes < n) {
+            if (myBufLen == 0) {
+                myBufLen = read4(myBuf);
+                myBufStart = 0;
+                if (myBufLen < 4) eof = true;
             }
-            if (eof || n == count) {
-                break;
-            } else {
-                int readCount = read4(tmpBuf);
-                tmpStart = 0;
-                tmpEnd = readCount;
-                if (readCount < 4) {
-                    eof = true;
-                }
-            }
-        } while (true);
-
-        return count;
+            int copiedBytes = min(myBufLen, n - readBytes);
+            memcpy(buf + bufStart, myBuf + myBufStart, copiedBytes);
+            myBufStart += copiedBytes;
+            myBufLen -= copiedBytes;
+            bufStart += copiedBytes;
+            readBytes += copiedBytes;
+        }
+        return readBytes;
     }
 };
