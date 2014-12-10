c5f82a2a251e84f1cda251a8c2a0957048e3d797
Jiankuan Xing <speedfirst@gmail.com>
2014-12-10 13:08:26 +0800

    renew 2D solution

--- cpp/WordBreak.cc
+++ cpp/WordBreak.cc
@@ -41,41 +41,27 @@ namespace WordBreak {
         }
     };
     
-    // 2-D dp
+    // 2-D dp, time complexity O(n^3), space complexity O(n^2)
     class Solution {
     public:
-        pair<int, int> findMinLenAndMaxLen(unordered_set<string> &dict) {
-            int minLen = INT_MAX;
-            int maxLen = INT_MIN;
-            for (unordered_set<string>::iterator it = dict.begin(); it != dict.end(); it++) {
-                const string & cur = *it;
-                minLen = min((int)cur.size(), minLen);
-                maxLen = max((int)cur.size(), maxLen);
-            }
-            return make_pair(minLen, maxLen);
-        }
-        
         bool wordBreak(string s, unordered_set<string> &dict) {
-            if (s.empty()) return false;
-            if (dict.empty()) return false;
-            
-            pair<int, int> m = findMinLenAndMaxLen(dict);
-            int minLen = m.first;
-            int maxLen = m.second;
-            vector<bool> dp = vector<bool>(s.size() + 1, false);
-            dp[0] = true;
-            for (int i = 1; i <= s.size(); i++) {
-                if (!dp[i - 1]) continue;
-                for (int len = minLen; len <= maxLen; len++) {
-                    if (i + len - 1 > s.size()) break;
-                    string cur = s.substr(i - 1, len);
-                    if (dict.find(cur) != dict.end()) {
-                        dp[i + len - 1] = true;
+            vector<vector<bool> > dp(s.size(), vector<bool>(s.size() + 1, false));
+            dp[0][0] = true;
+            for (int len = 1; len <= s.size(); len++) {
+                for (int i = 0; i <= s.size() - len; i++) {
+                    if (dict.find(s.substr(i, len)) != dict.end()) {
+                        dp[i][len] = true;
+                        continue;
+                    }
+                    for (int k = 1; k < len; k++) {
+                        if (dp[i][k] && dp[i + k][len - k]) {
+                            dp[i][len] = true;
+                            break;
+                        }
                     }
                 }
             }
-            
-            return dp[s.size()];
+            return dp[0][s.size()];
         }
     };
 }
