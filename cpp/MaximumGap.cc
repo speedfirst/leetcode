0faba3d8f11ee466b9a3c1ec8466e5827fffe308
Jiankuan Xing <speedfirst@gmail.com>
2014-12-16 00:52:28 +0800

    Update MaximumGap.cc

--- cpp/MaximumGap.cc
+++ cpp/MaximumGap.cc
@@ -21,3 +21,46 @@ public:
         return maxGap;
     }
 };
+
+// official bucket based solution
+class Solution {
+public:
+    int maximumGap(vector<int> &num) {
+        if (num.size() < 2) return 0;
+        
+        int minValue = INT_MAX;
+        int maxValue = 0;
+        for (int i = 0; i < num.size(); i++) {
+            minValue = min(minValue, num[i]);
+            maxValue = max(maxValue, num[i]);
+        }
+        // note, the "double" & "ceil" is very important!!!
+        int bucketRange = ceil(((double)(maxValue - minValue) / (num.size() - 1))); 
+        int bucketNum = (maxValue - minValue) / bucketRange + 1;
+        vector<int> lowers(bucketNum, -1); // -1 means it's empty
+        vector<int> uppers(bucketNum, -1);
+        
+        for (int i = 0; i < num.size(); i++) {
+            int bucketPos = (num[i] - minValue) / bucketRange;
+            if (lowers[bucketPos] == -1) {
+                lowers[bucketPos] = num[i];
+                uppers[bucketPos] = num[i];
+            } else {
+                lowers[bucketPos] = min(lowers[bucketPos], num[i]);
+                uppers[bucketPos] = max(uppers[bucketPos], num[i]);
+            }
+        }
+        
+        // iterate the buckets to get the max gap
+        assert(uppers[0] != -1 && lowers[bucketNum - 1] != -1);
+        int maxGap = 0;
+        int prevUpper = uppers[0];
+        for (int i = 1; i < bucketNum; i++) {
+            if (lowers[i] == -1) continue;
+            maxGap = max(maxGap, lowers[i] - prevUpper);
+            prevUpper = uppers[i];
+        }
+        
+        return maxGap;
+    }
+};
