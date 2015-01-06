8179d1b321118c1239579d398ab5f9106429c84c
Jiankuan Xing <speedfirst@gmail.com>
2015-01-06 12:11:46 +0800

    Update BinaryTreeLevelOrderTraversal.cc

--- cpp/BinaryTreeLevelOrderTraversal.cc
+++ cpp/BinaryTreeLevelOrderTraversal.cc
@@ -1,5 +1,6 @@
 // https://oj.leetcode.com/problems/binary-tree-level-order-traversal/
 namespace BinaryTreeLevelOrderTraversal {
+    // BFS based solution. Space O(n), Time O(n)
     class Solution {
     public:
         vector<vector<int> > levelOrder(TreeNode *root) {
@@ -41,6 +42,43 @@ namespace BinaryTreeLevelOrderTraversal {
             
             return res;
             
+        }
+    };
+    
+    // recursion based solution. Space complexity O(1) (if stack is not count in), time complexity is O(n),
+    // or more clear 2^(h + 1) - 2 - h, where h is the height of the tree.
+    class Solution2 {
+    public:
+        void traverse(TreeNode * curNode, vector<int> &curLevel, int curHeight, int targetHeight) {
+            if (curNode == NULL) {
+                return;
+            }
+            
+            if (curHeight == targetHeight) {
+                curLevel.push_back(curNode->val);
+                return;
+            }
+            
+            traverse(curNode->left, curLevel, curHeight + 1, targetHeight);
+            traverse(curNode->right, curLevel, curHeight + 1, targetHeight);
+        }
+        
+        vector<vector<int> > levelOrder(TreeNode *root) {
+            vector<vector<int> > res;
+            
+            int targetHeight = 1;
+            while (true) {
+                vector<int> curLevel;
+                traverse(root, curLevel, 1, targetHeight);
+                if (curLevel.empty()) {
+                    break;
+                } else {
+                    res.push_back(curLevel);
+                }
+                targetHeight++;
+            }
+            
+            return res;
         }
     };
 }
